2aaeeffb66762c266ebd091fe63c0f336eca486a
Francisco Reyne <48486250+FranciscoReyne@users.noreply.github.com>
2025-06-19 20:51:01 -0400

    Create nympu_v1.py

---
+++ nympu_v1.py
@@ -0,0 +1,8 @@
+# v1
+
+# Crear dataset de sumas posibles
+sum_cache = {(i, j): i + j for i in range(-1000, 1001) for j in range(-1000, 1001)}Add commentMore actions
+
+def suma_rapida(a, b):
+    return sum_cache.get((a, b), a + b)  # En caso de no encontrar, calcula directamente
+
